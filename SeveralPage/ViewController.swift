6f43054fdf127ca96944e80d572c32564ef858d4
Siddiqjon <kuziboevsiddiqjon98@gmail.com>
2023-09-23 23:35:42 +0500

    Initial Commit

---
+++ SeveralPage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeveralPage
+//
+//  Created by Kuziboev Siddikjon on 23/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
